8a6871ca92d2d74f3bf5ef177e1b7889520e1f12
Dinu-Senal <60167570+Dinu-Senal@users.noreply.github.com>
2022-02-09 17:59:49 +0530

    program id configuration

--- programs/actium/src/lib.rs
+++ programs/actium/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("4MzsanEXKmTtjA3t9eLDC7YUfAyf8Wp9m9vTKTvPSSqL");
 
 #[program]
 pub mod actium {
